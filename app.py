2e97950588d3935f7e9e508668887b2e9cb9ce79
souloper <cse2019026@rcciit.org.in>
2022-06-05 16:40:45 +0530

    Updated UI

--- app.py
+++ app.py
@@ -48,7 +48,7 @@ class BlogPost(db.Model):
     title = db.Column(db.String(100), nullable=False)
     content = db.Column(db.Text, nullable=False)
     author = db.Column(db.String(20), nullable=False, default='N/A')
-    date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    date_posted = db.Column(db.DateTime, nullable=False, default=datetime.now())
 
 
     def __repr__(self):
